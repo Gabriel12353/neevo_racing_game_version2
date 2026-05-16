17b9972c58f76729ba87fc6c3792532b6b1ffc7a
Gabriel12353 <2016069@std.clip.pt>
2026-05-16 13:15:55 +0100

    Fix root route by disabling static index

--- server.js
+++ server.js
@@ -11,7 +11,7 @@ const app = express();
 const server = http.createServer(app);
 const io = new Server(server);
 
-app.use(express.static(path.join(__dirname, "public")));
+app.use(express.static(path.join(__dirname, "public"), { index: false }));
 app.use(express.json());
 
 app.get("/", (req, res) => {
